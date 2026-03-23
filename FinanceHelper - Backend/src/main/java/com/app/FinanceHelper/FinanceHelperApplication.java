40b6699628cf15227fcf8899aac8747e49705e0a
QTZhenrique <78319388+QTZhenrique@users.noreply.github.com>
2026-03-23 14:37:04 +0000

    Initial commit
    Initial struct

---
+++ FinanceHelper - Backend/src/main/java/com/app/FinanceHelper/FinanceHelperApplication.java
@@ -0,0 +1,13 @@
+package com.app.FinanceHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceHelperApplication.class, args);
+	}
+
+}
---
+++ FinanceHelper - Backend/src/test/java/com/app/FinanceHelper/FinanceHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.FinanceHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
